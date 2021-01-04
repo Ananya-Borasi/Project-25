bcdc28db6fd15867afe0c4e2dbd71b5d68b87548
Ananya-Borasi <73879874+Ananya-Borasi@users.noreply.github.com>
2021-01-04 17:49:29 +0530

    Add files via upload

---
+++ PaperBall.js
@@ -0,0 +1,28 @@
+class PaperBall{
+
+    constructor(x,y,r){
+
+        var options={
+            isStatic: false,
+            restitution: 0.3,
+            friction: 0.5,
+            density: 1.2
+        }
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+    }
+    display(){
+
+        var paperPos = this.body.position;
+
+        push();
+        translate(paperPos.x, paperPos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        image(this.image,0,0,this.width,this.hieght);
+        pop();
+    }
+    }   
